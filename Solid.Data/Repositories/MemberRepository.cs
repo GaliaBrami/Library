43f1c2cd774530070bb7e0eec4683b7a86c3d257
GaliaBrami <galiabrami16@gmail.com>
2024-01-01 22:59:05 +0200

    ef1

---
+++ Solid.Data/Repositories/MemberRepository.cs
@@ -0,0 +1,21 @@
+﻿using Solid.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using Library.Entities;
+
+namespace Solid.Data.Repositories
+{
+    public class MemberRepository : IMemberRepository
+    {
+        private readonly DataContext _context;
+        public List<Member> GetMembers()
+        {
+            return _context.Members;
+        }
+    
+}
+}
